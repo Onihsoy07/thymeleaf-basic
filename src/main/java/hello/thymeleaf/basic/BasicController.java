dc08c4a6c41dba1fb62030d0dcef7f1dc9b48c53
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-06-19 23:28:42 +0900

    기본, 편의 객체 사용하기

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -2,11 +2,13 @@ package hello.thymeleaf.basic;
 
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -47,6 +49,19 @@ public class BasicController {
         model.addAttribute("userMap", userMap);
 
         return "basic/variable";
+    }
+
+    @GetMapping("/basic-objects")
+    public String basicObject(HttpSession session) {
+        session.setAttribute("sessionData", "hello session");
+        return "basic/basic-objects";
+    }
+
+    @Component("helloBean")
+    static class HelloBean {
+        public String hello(String data) {
+            return "Hello " + data;
+        }
     }
 
     @Data
