0564d2e21908e2c1e779b0a72d61114940a0e370
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-06-21 23:20:36 +0900

    부분 layout

--- src/main/java/hello/thymeleaf/basic/TemplateComtroller.java
+++ src/main/java/hello/thymeleaf/basic/TemplateComtroller.java
@@ -12,4 +12,9 @@ public class TemplateComtroller {
     public String template() {
         return "template/fragment/fragmentMain";
     }
+
+    @GetMapping("/layout")
+    public String layout() {
+        return "template/layout/layoutMain";
+    }
 }
