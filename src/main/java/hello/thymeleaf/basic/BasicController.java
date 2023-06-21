d16b0e4ded5faea6a6ef90a217c40451f0a676ab
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-06-21 22:39:40 +0900

    inline javascrtipt

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -130,6 +130,14 @@ public class BasicController {
         return "basic/block";
     }
 
+    @GetMapping("/javascript")
+    public String javascript(Model model) {
+        model.addAttribute("gg", "ggggggggg");
+        model.addAttribute("user", new User("user1", 10));
+        addUsers(model);
+        return "basic/javascript";
+    }
+
     @Data
     static class User {
         private String username;
